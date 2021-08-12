402379efa9aca9ce7a076645774f21ec2e98ed18
markgordon22 <markgordon97@gmail.com>
2021-08-12 00:24:31 +0000

    sorting and product counts to products page

--- products/views.py
+++ products/views.py
@@ -21,7 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -64,3 +65,4 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
