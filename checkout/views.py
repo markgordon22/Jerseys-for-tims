8c42bc41829eb6fdd880357905fd4e5dd1d79e3f
markgordon22 <markgordon97@gmail.com>
2021-08-15 13:26:06 +0000

    testing wh stripe endpoint - TDD

--- checkout/views.py
+++ checkout/views.py
@@ -75,7 +75,7 @@ def checkout(request):
                         "Please call us for assistance!")
                     )
                     order.delete()
-                    return redirect(reverse('view_bag'))
+                    return redirect(reverse('checkout'))
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
